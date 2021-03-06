37781c903e50ae7d48d6699190a2a707e671a2af
Ramiro Farias <ramirofarias.dev@gmail.com>
2021-03-06 19:54:18 -0300

    Fixed typos @ mergesort tests

--- src/SortingVisualizer/__tests/MergeSort.test.ts
+++ src/SortingVisualizer/__tests/MergeSort.test.ts
@@ -1,13 +1,14 @@
 import { MergeSort } from "../MergeSort";
 
 describe("MergeSort tests", () => {
+  const setBars = jest.fn();
   let arr = [2, 6, 5, 1, 3, 4];
   let sortedArr = [1, 2, 3, 4, 5, 6];
   it("Should return an array", () => {
-    expect(Array.isArray(MergeSort(arr))).toBe(true);
+    expect(Array.isArray(MergeSort(arr, 0, arr.length, setBars))).toBe(true);
   });
 
   it("Should return a sorted array", () => {
-    expect(MergeSort(arr)).toStrictEqual(sortedArr);
+    expect(MergeSort(arr, 0, arr.length, setBars)).toStrictEqual(sortedArr);
   });
 });
